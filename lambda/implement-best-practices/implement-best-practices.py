4ddb083f9698ff213e979ff1f0d3702a1663e557
Chris Chalfant <cchalfant@leafsoftwaresolutions.com>
2016-06-27 11:42:17 -0400

    remove rules from default sgs in all regions

--- lambda/implement-best-practices/implement-best-practices.py
+++ lambda/implement-best-practices/implement-best-practices.py
@@ -1,20 +1,33 @@
 import boto3
 import logging
+import json
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 # logger.setLevel(logging.DEBUG)
 
+def ec2_regions():
+  ec2 = boto3.client('ec2')
+  resp = ec2.describe_regions()
+  names = []
+  for region in resp['Regions']:
+    names.append(region['RegionName'])
+  return names
+
 
 # This is the method that will be registered
 # with Lambda and run on a schedule
 def handler(event={}, context={}):
-  ec2 = boto3.resource('ec2')
-  for sg in ec2.security_groups.filter(Filters=[{"Name":"group-name","Values": ["default"]}]).all():
-    for ingress in sg.ip_permissions:
-      sg.revoke_ingress(GroupId=sg.group_id,IpPermissions=[ingress])
-    for egress in sg.ip_permissions_egress:
-      sg.revoke_egress(GroupId=sg.group_id,IpPermissions=[egress])
+  for region in ec2_regions():
+    ec2 = boto3.resource('ec2', region_name=region)
+    logger.info('Checking {} for default SGs'.format(region))
+    for sg in ec2.security_groups.filter(Filters=[{"Name":"group-name","Values": ["default"]}]).all():
+      for ingress in sg.ip_permissions:
+        logger.info('Revoking ingress rule {} from {}'.format(json.dumps(ingress), sg.group_id))
+        sg.revoke_ingress(GroupId=sg.group_id,IpPermissions=[ingress])
+      for egress in sg.ip_permissions_egress:
+        logger.info('Revoking egress rule {} from {}'.format(json.dumps(egress), sg.group_id))
+        sg.revoke_egress(GroupId=sg.group_id,IpPermissions=[egress])
 
 # If being called locally, just call handler
 if __name__ == '__main__':
